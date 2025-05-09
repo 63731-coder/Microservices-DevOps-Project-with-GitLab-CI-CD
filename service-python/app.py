158a5e505e19c0982e70513d49aecaabb7748e6e
Opre Nicoleta <63731@etu.he2b.be>
2025-05-09 13:02:03 +0200

    partie 2

--- service-python/app.py
+++ service-python/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+import os
+
 
 app = Flask(__name__)
 
@@ -7,5 +9,5 @@ def message():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
